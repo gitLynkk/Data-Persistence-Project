c70eeaaa294bc66b81fa8029b330b0b1cf1f460b
Lynk <dan.cerardi17@gmail.com>
2022-09-01 22:42:01 +0200

    Menu Handler

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUIHandler : MonoBehaviour
+{
+
+    public void startNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+        #else
+            Application.Quit();
+        #endif
+    }
+
+}
